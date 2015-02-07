cae74f6b15c4892f32e55f3b7c73fc489c1c6e75
Ken Steiner <kenosteiner@gmail.com>
2015-02-07 21:21:27 +0100

    changes to working on heroku

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = {
-	url : 'mongodb://127.0.0.1:27017/skeleton',
+	url : 'mongodb://skeleton:skeleton@ds041871.mongolab.com:41871/skeleton',
 	testUrl : 'mongodb://127.0.0.1:27017/skeleton-test'
 }
